35a57d2084df52b4bbf30ed4e3ae62a812f61c89
javiercastellonvillanueva <javiercastellonvillanueva@gmail.com>
2024-01-02 17:43:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function addTask() {
+    let taskInput = document.getElementById("new-task");
+    let tasksList = document.getElementById("tasks");
+
+    if (taskInput.value === '') {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let newTask = document.createElement("li");
+    newTask.textContent = taskInput.value;
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = function() {
+        tasksList.removeChild(newTask);
+    };
+
+    newTask.appendChild(deleteButton);
+    tasksList.appendChild(newTask);
+
+    taskInput.value = "";
+}
